d318c964e414a0bbf444588e01e2fb3dc7f59d87
nopantsfriday <jens@nopantsfriday.de>
2022-03-18 18:47:07 +0100

    initial commit

---
+++ restart-steam.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+    Restarting Valve's steam client in case it's not loading the GUI or frozen.
+
+.DESCRIPTION
+    The script will search a running process "steam.exe", send a shutdown command "steam.exe -shutdown" and restart steam by using the steam protocol handler "steam:".
+
+.PARAMETER Param1
+    No parameters yet.
+
+.EXAMPLE
+    Example syntax for running the script or function
+    PS C:\> restart-steam.ps1
+.LINK
+    https://github.com/nopantsfriday/restart_steam_client
+
+.NOTES
+    Filename: restart-steam.ps1
+    Author: https://github.com/nopantsfriday
+    Modified date: 2022-03-18
+    Version 1.0 - Initial release without any try catch and steam procotol check as it works flawlessly on the developers machine.
+#>
+
+$steam_running = Get-Process -name steam -ErrorAction SilentlyContinue
+if ($steam_running) {
+
+  $steam = ( get-process -name steam).path 
+  Write-Host "Found Steam client:"; Write-Host $steam -BackgroundColor Black -ForegroundColor Cyan
+  &$steam -shutdown
+  Write-Host "Sending shutdown command:"; Write-Host $steam "-shutdown" -BackgroundColor Black -ForegroundColor Cyan
+  Write-Host "Starting Steam in 5 seconds" -ForegroundColor DarkYellow
+  Start-Sleep -s 5
+  &$steam
+}
+if (!$steam_running) {
+  Write-Host "Steam not running. Trying to start steam." -ForegroundColor Red
+  Start-Process steam:
+}
+Write-Host "Steam started" -ForegroundColor Green
