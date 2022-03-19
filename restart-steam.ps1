01d28cfde1b597c7cc0a9f5d4ae67771a07aab59
nopantsfriday <jens@nopantsfriday.de>
2022-03-19 22:05:06 +0100

    Merge branch 'main' of https://github.com/nopantsfriday/restart_steam_client

--- restart-steam.ps1
+++ restart-steam.ps1
@@ -18,23 +18,31 @@
 .NOTES
     Filename: restart-steam.ps1
     Author: https://github.com/nopantsfriday
-    Modified date: 2022-03-18
+    Modified date: 2022-03-19
     Version 1.0 - Initial release without any try catch and steam procotol check as it works flawlessly on the developers machine.
+    Versiion 1.0.1 - Check if Steam protocol handler is registered.
 #>
 
 $steam_running = Get-Process -name steam -ErrorAction SilentlyContinue
 if ($steam_running) {
 
-  $steam = ( get-process -name steam).path 
-  Write-Host "Found Steam client:"; Write-Host $steam -BackgroundColor Black -ForegroundColor Cyan
-  &$steam -shutdown
-  Write-Host "Sending shutdown command:"; Write-Host $steam "-shutdown" -BackgroundColor Black -ForegroundColor Cyan
-  Write-Host "Starting Steam in 5 seconds" -ForegroundColor DarkYellow
-  Start-Sleep -s 5
-  &$steam
+    $steam = ( get-process -name steam).path 
+    Write-Host "Found Steam client: " -NoNewline; Write-Host $steam -BackgroundColor Black -ForegroundColor Cyan
+    &$steam -shutdown
+    Write-Host "Sending shutdown command: " -NoNewline; Write-Host $steam "-shutdown" -BackgroundColor Black -ForegroundColor Cyan
+    Write-Host "Starting Steam in 5 seconds." -ForegroundColor DarkYellow
+    Start-Sleep -s 5
+    Write-Host "Starting Steam." -ForegroundColor Green
+    &$steam
+    
 }
 if (!$steam_running) {
-  Write-Host "Steam not running. Trying to start steam." -ForegroundColor Red
-  Start-Process steam:
+    if ((Test-Path -Path "registry::HKEY_CLASSES_ROOT\steam\Shell\Open\Command")) {
+        Write-Host "Steam not running. Starting Steam." -ForegroundColor Green
+        Start-Process steam:
+    }
+    else {
+        Write-Host "Steam is not running and the Steam protocol handler could not be found. Pleas start Steam manually." -ForegroundColor Red
+        Start-Sleep -s 5
+    }
 }
-Write-Host "Steam started" -ForegroundColor Green
